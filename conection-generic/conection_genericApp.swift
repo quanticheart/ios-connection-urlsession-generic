0ca20e284bf9e762b436ff98645679fbeb356bf4
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-03-11 21:11:09 -0300

    Initial Commit

---
+++ conection-generic/conection_genericApp.swift
@@ -0,0 +1,17 @@
+//
+//  conection_genericApp.swift
+//  conection-generic
+//
+//  Created by Jonn Alves on 11/03/23.
+//
+
+import SwiftUI
+
+@main
+struct conection_genericApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
